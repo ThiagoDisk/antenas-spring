1992d4e2300efacb8d805370618b9e29e4b727c7
RoneFB <ronefelipe97@gmail.com>
2020-02-29 10:35:44 -0300

    init spring-boot

---
+++ src/main/java/com/antenas/antenas/AntenasApplication.java
@@ -0,0 +1,13 @@
+package com.antenas.antenas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntenasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntenasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antenas/antenas/AntenasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antenas.antenas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntenasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
