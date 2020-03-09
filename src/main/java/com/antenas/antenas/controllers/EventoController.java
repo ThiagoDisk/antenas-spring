f25c53065f6b0104dc01e7edf87328905ab9369d
RoneFB <rone.bento@fatec.sp.gov.br>
2020-03-09 19:10:31 -0300

    deploy

--- src/main/java/com/antenas/antenas/controllers/EventoController.java
+++ src/main/java/com/antenas/antenas/controllers/EventoController.java
@@ -1,7 +1,12 @@
 package com.antenas.antenas.controllers;
 
+import java.io.IOException;
+import java.io.Writer;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class EventoController {
@@ -9,5 +14,11 @@ public class EventoController {
 	@RequestMapping("/cadastrarEvento")
 	public String form() {
 		return "evento/formEvento";
+	}
+	
+	@RequestMapping(path = "/something", method = RequestMethod.POST)
+	public void handle(@RequestBody String body, Writer writer) throws IOException {
+	    writer.write(body);
+	    System.out.println(body);
 	}
 }
